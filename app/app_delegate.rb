c44021a87f5f907b63a70e3c93229972875847bf
poc7667 <poc7667@gmail.com>
2015-04-22 14:28:06 +0800

    use tab_bar in app_delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,7 +3,8 @@ class AppDelegate < PM::Delegate
 
   def on_load(app, options)
     return true if RUBYMOTION_ENV == "test"
-    open HomeScreen.new(nav_bar: true)
+    # open HomeScreen.new(nav_bar: true)
+    open_tab_bar ReservoirsScreen, HelpScreen.new(nav_bar: true)
   end
 
 end
