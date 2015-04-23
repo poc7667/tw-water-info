a22481b8d0eba3dc3057bd3f9f4191c75d7a28ab
poc7667 <poc7667@gmail.com>
2015-04-23 09:29:58 +0800

    add tab bar in app_delegate.rb

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,10 +1,10 @@
 class AppDelegate < PM::Delegate
-  status_bar true, animation: :none
+  # status_bar true, animation: :none
 
   def on_load(app, options)
     return true if RUBYMOTION_ENV == "test"
     # open HomeScreen.new(nav_bar: true)
-    open_tab_bar ReservoirsScreen, HelpScreen.new(nav_bar: true)
+    open_tab_bar ReservoirsScreen.new(nav_bar: true), HelpScreen.new(nav_bar: true)
   end
 
 end
